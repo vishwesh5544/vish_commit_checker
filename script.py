f66007693591420c2ee693bb878021f48905f304
vishwesh5544 <vishweshshukla20@gmail.com>
2024-07-14 10:06:01 +0530

    added timer

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from command_runner import CommandRunner
 from vish_commit_checker import VishCommitChecker
+import time
 
 
 if __name__ == "__main__":
@@ -8,12 +9,15 @@ if __name__ == "__main__":
     working_dir = "/var/www/html/devops-cicd"
     
     checker = VishCommitChecker(repo_owner, repo_name)
-    new_commits = checker.check_new_commits()
+    runner = CommandRunner(working_dir)
     
-    if new_commits:
-        print("New commits found. Pulling changes and reloading Nginx.")
-        runner = CommandRunner(working_dir)
-        runner.pull_and_reload()
-        print("Deployment complete.")
-    else: 
-        print("No new commits found.")
+    while True:
+        if checker.check_new_commits():
+            print("New commits found. Pulling changes and reloading Nginx.")
+            runner.pull_and_reload()
+            print("Deployment complete.")
+        else: 
+            print("No new commits found.")
+        
+        time.sleep(60)
+    
